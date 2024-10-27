b2c97ce5baca2bbde0b4c4027c633a4924bfdf72
toothlmao-03 <oli.tomas07@gmail.com>
2024-10-27 11:53:07 +0000

    HAFSDF

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var form = document.querySelector("form");
-
-  if (localStorage.getItem('reserved_futsal')){
-    document.getElementById('complete-reservation-futsal').disabled = true;
-  }
-
-  form.addEventListener("submit", function(event) {
-    event.preventDefault();
-
-    var currentPath = window.location.pathname;
-    if (currentPath.includes("reservas.html")) {
       if (localStorage.getItem('reserved')){
         document.getElementById('complete-reservation').disabled = true;
       } else {
@@ -44,6 +33,9 @@ document.addEventListener("DOMContentLoaded", function() {
               console.log("Reserva de "+tickets+" para "+name+" "+surname+" concluída")
               window.location.href = 'success.html';
               localStorage.setItem('reserved', true);
+              document.getElementById('complete-reservation').disabled = true;
+              getElementById('complete-reservation').value = "A carregar..."
+
             } else {
               alert('Erro ao realizar reserva. Por favor, tenta novamente.')
             }
@@ -54,76 +46,6 @@ document.addEventListener("DOMContentLoaded", function() {
           });
         });
       }
-      } else
-        
-      
-      if (localStorage.getItem('reserved_futsal')){
-        document.getElementById('complete-reservation-futsal').disabled = true;
-      } else {
-          document.getElementById('complete-reservation-futsal').addEventListener('click', function() {
-            document.getElementById('complete-reservation-futsal').disabled = true;
-            document.getElementById('complete-reservation-futsal').textContent = "A carregar..."
-            console.log("abc")
-            var team_name = document.querySelector('input[name="team-name"]').value
-            var member1 = document.querySelector('input[name="name1').value + " " + document.querySelector('input[name="class1').value
-            var member2 = document.querySelector('input[name="name2').value + " " + document.querySelector('input[name="class2').value
-            var member3 = document.querySelector('input[name="name3').value + " " + document.querySelector('input[name="class3').value
-            var member4 = document.querySelector('input[name="name4').value + " " + document.querySelector('input[name="class4').value
-            var member5 = document.querySelector('input[name="name5').value + " " + document.querySelector('input[name="class5').value
-      
-            var formData = new FormData();
-            formData.append('type', 'futsal')
-            formData.append('team_name', team_name)
-            formData.append('member1', member1)
-            formData.append('member2', member2)
-            formData.append('member3', member3)
-            formData.append('member4', member4)
-            formData.append('member5', member5)
-      
-            if(document.querySelector('input[name="name6"]').value) {
-              var member6 = document.querySelector('input[name="name6"]').value + " " + document.querySelector('input[name="class6"]').value;
-              formData.append('member6', member6);
-            }
-            if(document.querySelector('input[name="name7"]').value) {
-              var member7 = document.querySelector('input[name="name7"]').value + " " + document.querySelector('input[name="class7"]').value;
-              formData.append('member7', member7);
-            }
-            if(document.querySelector('input[name="name8"]').value) {
-              var member8 = document.querySelector('input[name="name8"]').value + " " + document.querySelector('input[name="class8"]').value;
-              formData.append('member8', member8);
-            }
-            if(document.querySelector('input[name="name9"]').value) {
-              var member9 = document.querySelector('input[name="name9"]').value + " " + document.querySelector('input[name="class9"]').value;
-              formData.append('member9', member9);
-            }
-            if(document.querySelector('input[name="name10"]').value) {
-              var member10 = document.querySelector('input[name="name10"]').value + " " + document.querySelector('input[name="class10"]').value;
-              formData.append('member10', member10);
-            }
-            if(document.querySelector('input[name="name11"]').value) {
-              var member11 = document.querySelector('input[name="name11"]').value + " " + document.querySelector('input[name="class11"]').value;
-              formData.append('member11', member11);
-            }
-      
-            fetch('https://script.google.com/macros/s/AKfycbzNyZHv_DbyDYpCZQuKYHAcQfzL8DIcVqVg6LcH4TaJs0QJ3_edgdlUctn8AIq_wu3w9w/exec', {
-              method: 'POST',
-              body: formData
-            })
-            .then(response => {
-              if (response.ok) {
-                window.location.href = 'success';
-                localStorage.setItem('reserved_futsal', true);
-              } else {
-                alert('Erro ao criar equipa. Por favor, tenta novamente.')
-              }
-            })
-            .catch(error => {
-              console.error('Erro ao fazer solicitação:', error);
-              alert('Erro ao criar equipa. Por favor, tenta novamente.')
-            });
-          })
-        }
-  });
 });
 
 
